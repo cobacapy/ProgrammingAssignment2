29c1b7057f119e15b6e69bdb96cddc7cd1d76776
cobacapy <89282137+cobacapy@users.noreply.github.com>
2021-08-20 16:51:01 -1000

    Add files via upload
    submission 8-20-2021

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-programming Course Assignment #2: Caching the inverse of a matrix
+## two functions one to generate the cache and a second to poll the cache and
+##pull the values if they are present
 
-## Write a short comment describing this function
+## the function below returns a list containing four functions
+## to define and retrieve the values of the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function recieves the above function as an input.
+## The cached values are polled, and if the result is present the cached values
+## are pulled.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+      message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
